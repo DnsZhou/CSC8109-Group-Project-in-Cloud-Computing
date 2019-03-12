96703a9fa508eefc8555973f3875166c4b825525
Dennis Zhou <zt1020@yeah.net>
2019-03-12 22:04:15 +0000

    delete unused functions and format the code

--- DevEnv/Web/js/transactionDetail.js
+++ DevEnv/Web/js/transactionDetail.js
@@ -151,21 +151,3 @@ function showPage(transactionData) {
     $("#mainContent").removeClass("hidden");
     $("#recieverName").text(transactionData.sender);
 }
-
-async function delayProcess() {
-    for (var percentage = 0; percentage <= 100; percentage++) {
-
-        setTimeout(function () {
-            $("#progressBar").attr("aria-valuenow", percentage);
-            $("#progressBar").css("width", percentage + "%");
-            if (percentage > 99) {
-                setTimeout(function () {
-                    $("#progressDone").removeClass("hidden");
-                    $("#onProgress").addClass("hidden");
-                }, 1000)
-            }
-        }, 1000);
-
-    }
-
-}
